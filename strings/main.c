9b9447fe7fbc13b5e4b3d3ddd4caf836ff2d1f01
Deepak Kundra <deepakkundra@gmail.com>
2016-02-29 19:49:17 -0800

    Initial Commit

---
+++ strings/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  strings
+//
+//  Created by Deepak Kundra on 2/29/16.
+//  Copyright © 2016 Deepak Kundra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
